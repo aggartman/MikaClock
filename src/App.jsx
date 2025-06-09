2221c07ec30a8dd09a5f41346391a589212a4cb4
Austin Rizer <aggrizer@gmail.com>
2025-06-09 12:38:15 -0500

    Update framework to Vite with React library to modernize

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import mikaBody from './assets/mikaBody.svg'
+import mikaTail from './assets/mikaTail.svg'
+import mikaEyes from './assets/mikaEyes.svg'
+import minuteHand from './assets/minuteHand.svg'
+import hourHand from './assets/hourHand.svg'
+import handCover from './assets/handCover.svg'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="appContainer">
+          <div className="clock">
+              <img className="clockBody" src={mikaBody}  alt="clock body of Mika Clock"/>
+              <img className="clockTail" src={mikaTail} alt="clockTail Tail"/>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
