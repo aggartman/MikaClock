2d1489dcc868af513aa1289dd8830f943e7fb0b2
Austin Rizer <aggrizer@gmail.com>
2025-06-09 22:30:46 -0500

    Update to include all clock parts

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,12 @@ function App() {
     <>
       <div className="appContainer">
           <div className="clock">
-              <img className="clockBody" src={mikaBody}  alt="clock body of Mika Clock"/>
+              <img className="clockEyes" src={mikaEyes} alt="Eyes of the clock" />
+              <img className="clockBody" src={mikaBody}  alt="clock body of Mika Clock emulation of the Kit-Cat Klock"/>
               <img className="clockTail" src={mikaTail} alt="clockTail Tail"/>
+              <img className="handCover" src={handCover} alt="clock hand cover"/>
+              <img className="minuteHand" src={minuteHand} alt="clock minute hand"/>
+              <img className="hourHand" src={hourHand} alt="clock hour hand"/>
           </div>
       </div>
     </>
